51f013fe13a0570eee31524e09ce0eca93caa4d9
rattahamberg <github.subject765@passmail.net>
2025-04-07 00:22:38 -0700

    modified spacing, changed image to .webp

--- src/components/navigation bar/Navbar.tsx
+++ src/components/navigation bar/Navbar.tsx
@@ -3,25 +3,27 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="font-francois-one bg-3d-orange-primary flex flex-row items-center p-5 text-4xl text-amber-50">
+    <nav className="font-francois-one bg-3d-orange-primary flex flex-row items-center px-5 py-2 text-4xl text-amber-50">
       <div className="flex w-1/2 items-center justify-start">
         <Image
-          src={"/Logo.png"}
+          src={"/Logo.webp"}
           alt={"Logo"}
           width={70}
           height={70}
           className="m-1 rounded-full border-2 border-amber-50"
         ></Image>
-        <Link href="/home">3D Club</Link>
+        <Link href="/home" className="px-5">
+          3D Club
+        </Link>
       </div>
-      <div className="flex w-1/2 justify-evenly">
-        <Link href="/about" className="">
+      <div className="flex w-1/2 justify-end">
+        <Link href="/about" className="px-10">
           ABOUT
         </Link>
-        <Link href="/board" className="">
+        <Link href="/board" className="px-10">
           BOARD
         </Link>
-        <Link href="/gallery" className="">
+        <Link href="/gallery" className="px-10">
           GALLERY
         </Link>
       </div>
