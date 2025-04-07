7c6cb4b063062a09551eb3f0cfdbc838aeed6b33
Phillip C <pcfan16@gmail.com>
2025-04-07 16:21:10 -0700

    White Title Component

---
+++ src/components/title.tsx
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <div>
+      <p className="text-size = 40px; text-decoration-line: underline; text-white">
+        What We Do:     </p>
+    </div>
+  );
+};
+export default Title;
