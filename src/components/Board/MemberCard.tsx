0011319da5e87c37fb3b50c2104751c898c4dd19
tarillo <tanya.carrillo707@gmail.com>
2025-04-07 13:12:16 -0700

    created the Member Card and placehodler for blue component

---
+++ src/components/Board/MemberCard.tsx
@@ -0,0 +1,14 @@
+
+const MemberCard = () => {
+    return (
+        <div className="w-60 h-80 bg-3d-red-primary shadow-3d-red-sceondary rounded-lg shadow-2xl p-2.5 flex items-center justify-center">
+            <div className="w-full h-50 border-4 border-white p-2 rounded-xs ">
+                <div className="w-full h-full border-gray-300 bg-gray-200 flex items-center justify-center"> {/*replace with image*/ }
+                    {/*blue component placeholder*/}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MemberCard;
