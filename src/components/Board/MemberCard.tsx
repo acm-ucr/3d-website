ed8c6dfb9168209f8b5774e67638bb5b45bc1dc5
tarillo <tanya.carrillo707@gmail.com>
2025-04-08 00:30:50 -0700

    added BoxShadow to Member Card

--- src/components/Board/MemberCard.tsx
+++ src/components/Board/MemberCard.tsx
@@ -1,14 +1,21 @@
+import BoxShadow from "@/components/BoxShadow";
+
 const MemberCard = () => {
   return (
-    <div className="bg-3d-red-primary shadow-3d-red-sceondary flex h-80 w-60 items-center justify-center rounded-lg p-2.5 shadow-2xl">
-      <div className="h-50 w-full rounded-xs border-4 border-white p-2">
-        <div className="flex h-full w-full items-center justify-center border-gray-300 bg-gray-200">
-          {" "}
-          {/*replace with image*/}
-          {/*blue component placeholder*/}
+    <BoxShadow
+      boxColor="bg-3d-red-primary"
+      boxShadow="shadow-3d-red-secondary rounded-sm"
+      boxPadding="pr-0.2 pl-0.2 py-3"
+    >
+      <div className="bg-3d-red-primary flex h-80 w-60 items-center justify-center rounded-lg p-2.5">
+        <div className="h-50 w-full rounded-xs border-4 border-white p-2">
+          <div className="flex h-full w-full items-center justify-center border-gray-300 bg-gray-200">
+            {/*replace with image*/}
+            {/*blue component placeholder*/}
+          </div>
         </div>
       </div>
-    </div>
+    </BoxShadow>
   );
 };
 
