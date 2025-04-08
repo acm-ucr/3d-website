14bbab607cf3bb79130623eb8030012646e75068
tarillo <tanya.carrillo707@gmail.com>
2025-04-08 00:45:28 -0700

    fixed the padding to be py-0

--- src/components/Board/MemberCard.tsx
+++ src/components/Board/MemberCard.tsx
@@ -5,7 +5,7 @@ const MemberCard = () => {
     <BoxShadow
       boxColor="bg-3d-red-primary"
       boxShadow="shadow-3d-red-secondary rounded-sm"
-      boxPadding="pr-0.2 pl-0.2 py-3"
+      boxPadding="pr-0.2 pl-0.2 py-0"
     >
       <div className="bg-3d-red-primary flex h-80 w-60 items-center justify-center rounded-lg p-2.5">
         <div className="h-50 w-full rounded-xs border-4 border-white p-2">
