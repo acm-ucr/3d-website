1212cb283e5164b866012dcf9292a9b0a9129edb
Draylend <draylend0821@gmail.com>
2025-04-16 21:50:41 -0700

    Edit NightForest.tsx for width
    * Edited the width of the box shadow so it didn't extend into a scroll bar

--- src/components/Gallery/NightForest.tsx
+++ src/components/Gallery/NightForest.tsx
@@ -12,7 +12,7 @@ const NightForest = () => {
         boxColor="bg-3d-red-primary"
         boxShadow="shadow-3d-red-secondary"
         boxPadding="px-8 py-3"
-        custom_style="absolute top-[10%] left-[42%] w-full text-5xl text-white z-50"
+        custom_style="absolute top-[10%] left-[42%] text-5xl text-white z-50"
       >
         Interested in seeing and learning <br />
         more about our models?
@@ -22,7 +22,7 @@ const NightForest = () => {
         boxColor="bg-3d-red-primary"
         boxShadow="shadow-3d-red-secondary"
         boxPadding="px-6 py-5"
-        custom_style="absolute bottom-[10%] left-[6%] w-full text-5xl text-white z-50 underline underline-offset-4"
+        custom_style="absolute bottom-[10%] left-[6%] text-5xl text-white z-50 underline underline-offset-4"
       >
         <Link
           href={
