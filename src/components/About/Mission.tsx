85c7c49c7483f681c3c59a020a7187a27c1eb8e8
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-04-21 16:18:57 -0700

    fixed bg aspect

--- src/components/About/Mission.tsx
+++ src/components/About/Mission.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 const Mission = () => {
   return (
     <div>
-      <div className="h-96 overflow-hidden flex items-center justify-center">
-        <Image 
-          src="/about/mission.png"
-          alt="Mission background"
-          width={500}
-          height={500}
-          className="h-auto w-full"
-        />
+      <div className="relative aspect-[2.35/1] w-full">
+        <div className="absolute inset-0 flex justify-center overflow-hidden">
+          <Image
+            src="/about/mission.png"
+            alt="Mission background"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover object-[center_45%]"
+          />
+        </div>
       </div>
     </div>
   );
