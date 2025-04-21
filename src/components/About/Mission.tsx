a0051da81b12b2b7249993d6f017026cb416007e
Henry <134236009+braindead-dev@users.noreply.github.com>
2025-04-21 16:24:17 -0700

    box shadow

--- src/components/About/Mission.tsx
+++ src/components/About/Mission.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import BoxShadow from "../BoxShadow";
 
 const Mission = () => {
   return (
-    <div>
+    <div className="relative">
       <div className="relative aspect-[2.35/1] w-full">
         <div className="absolute inset-0 flex justify-center overflow-hidden">
           <Image
@@ -13,6 +14,14 @@ const Mission = () => {
             className="h-full w-full object-cover object-[center_45%]"
           />
         </div>
+        <BoxShadow
+          boxColor="bg-3d-blue-primary"
+          boxShadow="shadow-3d-blue-secondary"
+          boxPadding="px-5 py-2"
+          custom_style="absolute top-[5%] left-[5%] text-5xl text-black z-10"
+        >
+          Our Mission:
+        </BoxShadow>
       </div>
     </div>
   );
