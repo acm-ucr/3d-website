cd011788ad563b8325d13e090a54efd5b0f229df
rattahamberg <github.subject765@passmail.net>
2025-04-23 14:40:26 -0700

    Initial interest section mobile fix

--- src/components/home/Interest.tsx
+++ src/components/home/Interest.tsx
@@ -4,12 +4,13 @@ import Background from "@/public/home/interest.webp";
 
 const Interest = () => (
   <div className="relative w-full">
-    <div className="relative h-[80vh] w-full overflow-hidden">
+    <div className="relative h-[60vh] w-full overflow-hidden sm:h-[80vh]">
       <Image
         src={Background}
         alt="A meeting of the 3D Club"
         fill
-        sizes="100vw"
+        sizes="(max-width: 640px) 100vw, 100vw"
+        priority
         className="object-cover object-center"
       />
     </div>
@@ -17,8 +18,10 @@ const Interest = () => (
     <BoxShadow
       boxColor="bg-3d-blue-primary"
       boxShadow="shadow-3d-blue-secondary"
-      boxPadding="p-[0.5vw]"
-      custom_style="absolute top-[10%] left-1/2 -translate-x-1/2 z-10 flex justify-center whitespace-nowrap text-[3vw]"
+      boxPadding="px-4 py-2 sm:p-[0.5vw]"
+      custom_style="absolute top-4 sm:top-[10%] left-1/2 -translate-x-1/2
+                    z-10 flex justify-center whitespace-nowrap
+                    text-[clamp(1rem,5vw,2rem)]"
     >
       Interested in becoming a member?
     </BoxShadow>
@@ -26,10 +29,13 @@ const Interest = () => (
     <BoxShadow
       boxColor="bg-3d-blue-primary"
       boxShadow="shadow-3d-blue-secondary"
-      boxPadding="px-[4vw]"
-      custom_style="absolute bottom-[5%] right-[5%] z-10 flex justify-center text-[4vw]"
+      boxPadding="px-6 py-3 sm:px-[4vw]"
+      custom_style="absolute bottom-4 sm:bottom-[5%] right-1/2 sm:right-[5%]
+                    translate-x-1/2 sm:translate-x-0
+                    z-10 flex justify-center
+                    text-[clamp(1.25rem,6vw,2.5rem)]"
     >
-      Join Us!
+      Join&nbsp;Us!
     </BoxShadow>
   </div>
 );
