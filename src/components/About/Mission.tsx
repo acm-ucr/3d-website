68af8de5e33cd78dba9ac2a46075d01540e9f391
ce <tom735593@gmail.com>
2025-05-11 21:29:36 -0700

    fixed formatting

--- src/components/About/Mission.tsx
+++ src/components/About/Mission.tsx
@@ -2,33 +2,37 @@ import Image from "next/image";
 import BoxShadow from "../BoxShadow";
 
 const Mission = () => {
-    return (
-      <div className="relative w-full">
-        <div className="relative w-full aspect-[2.35/1] sm:aspect-[2.35/1] aspect-[1/1.2]">
-          <div className="absolute inset-0 flex justify-center overflow-hidden">
-            <Image
-              src="/about/mission.png"
-              alt="Mission background"
-              width={500}
-              height={500}
-              className="h-full w-full object-cover object-[center_45%]"
-            />
-          </div>
-  
-          <div className="absolute inset-0 flex flex-col sm:block items-center justify-center sm:justify-start px-4">
+  return (
+    <div className="relative w-full">
+      <div className="relative aspect-[1/1.2] aspect-[2.35/1] w-full sm:aspect-[2.35/1]">
+        <div className="absolute inset-0 flex justify-center overflow-hidden">
+          <Image
+            src="/about/mission.png"
+            alt="Mission background"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover object-[center_45%]"
+          />
+        </div>
+
+        <div className="absolute inset-0">
+          <div className="absolute top-[5%] left-[5%] z-10 max-w-[80%] sm:max-w-none">
             <BoxShadow
               boxColor="bg-3d-blue-primary"
               boxShadow="shadow-3d-blue-secondary"
-              boxPadding="px-4 py-2"
-              custom_style="text-xl sm:text-3xl md:text-4xl lg:text-5xl text-black z-10 mb-4 sm:mb-0 max-w-[90%] sm:max-w-none sm:absolute sm:top-[5%] sm:left-[5%]"
+              boxPadding="px-2 py-1 sm:px-4 sm:py-2"
+              custom_style="text-xs sm:text-xl md:text-2xl text-black"
             >
               Our Mission:
             </BoxShadow>
+          </div>
+
+          <div className="absolute right-[5%] bottom-[7%] z-10 max-w-[60%] text-left sm:max-w-[50%] sm:text-right md:max-w-[45%]">
             <BoxShadow
               boxColor="bg-3d-blue-primary"
               boxShadow="shadow-3d-blue-secondary"
-              boxPadding="px-4 py-2"
-              custom_style="text-base sm:text-xl md:text-2xl lg:text-3xl text-black text-center z-10 max-w-[90%] sm:max-w-none sm:absolute sm:bottom-[7%] sm:right-[5%]"
+              boxPadding="px-2 py-1 sm:px-4 sm:py-2"
+              custom_style="text-xs sm:text-base md:text-xl text-black"
             >
               We aim to create a <br />
               supportive environment <br />
@@ -39,8 +43,8 @@ const Mission = () => {
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Mission;
-  
+    </div>
+  );
+};
+
+export default Mission;
