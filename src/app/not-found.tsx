fd0c8a50603a539a7a6760da186ccc1010a6a43d
Aryan Goel <aryangoel27@gmail.com>
2025-06-20 15:31:39 -0700

    more edits pt2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,26 +4,30 @@ import BoxShadow from "@/components/box_shadow";
 const NotFound = () => {
   return (
     <div className="bg-3d-orange-secondary flex h-screen flex-col items-center justify-center space-y-10">
-      <BoxShadow
-        textSize="text-[8vw] sm:text-[4vw] text-amber-50"
-        boxShadow="shadow-3d-red-secondary"
-        boxPadding="px-[4vw]"
-        boxColor="bg-3d-red-primary"
-        custom_style="flex justify-center text-center"
-      >
-        404
-        <hr className="border-0" />
-        Page Not Found
-      </BoxShadow>
-      <BoxShadow
-        boxColor="bg-3d-blue-primary"
-        boxShadow="shadow-3d-blue-secondary"
-        boxPadding="p-[1vh]"
-        textSize="text-[3vw] sm:text-[1.25vw]"
-        custom_style="flex justify-center"
-      >
-        <Link href="/">Back To Home</Link>
-      </BoxShadow>
+      <div className="flex justify-center">
+        <BoxShadow
+          boxShadow="shadow-3d-red-secondary"
+          boxPadding="px-[4vw]"
+          boxColor="bg-3d-red-primary"
+        >
+          <div className="text-center text-[8vw] text-amber-50 sm:text-[4vw]">
+            <p>404</p>
+            <hr className="border-0" />
+            <p>Page Not Found</p>
+          </div>
+        </BoxShadow>
+      </div>
+      <div className="flex justify-center">
+        <BoxShadow
+          boxColor="bg-3d-blue-primary"
+          boxShadow="shadow-3d-blue-secondary"
+          boxPadding="p-[1vh]"
+        >
+          <Link href="/">
+            <p className="text-[3vw] sm:text-[1.25vw]">Back To Home</p>
+          </Link>
+        </BoxShadow>
+      </div>
     </div>
   );
 };
