dae64b0c3253bde820f4e196eb784675b334f30e
Mailyn <ngu.mailyn.yen@gmail.com>
2025-06-20 20:16:01 -0700

    mobile response about us

--- src/components/about/landing.tsx
+++ src/components/about/landing.tsx
@@ -5,19 +5,19 @@ import Image from "next/image";
 
 const Landing = () => {
   return (
-    <div className="flex flex-row p-[3%]">
-      <div className="mt-[4%] mb-[2%] ml-[4%]">
-        <Title textSize="text-5xl" customStyle="ml-[4%]">
+    <div className="flex flex-col-reverse p-[6%] xl:flex-row xl:p-[3%]">
+      <div className="mx-[4%] mt-[4%] mb-[2%] xl:ml-[4%]">
+        <Title textSize="text-2xl xl:text-5xl" customStyle="ml-[4%]">
           About Us:
         </Title>
 
         <BoxShadow
           boxColor="bg-3d-red-primary"
           boxShadow="shadow-3d-red-secondary"
-          boxPadding="p-[2%]"
-          customStyle="mt-[5%] w-3/4"
+          boxPadding="p-[4%] xl:p-[2%]"
+          customStyle="mt-[5%] w-full xl:w-3/4"
         >
-          <p className="text-4xl leading-snug text-white">
+          <p className="text-lg leading-snug text-white xl:text-4xl">
             We are a 3D art organization that aims to explore all types of 3D
             art media from computer software, to Minecraft builds, clay, and
             much more!
@@ -25,7 +25,11 @@ const Landing = () => {
         </BoxShadow>
       </div>
 
-      <Image src={LandingImg} alt="about_us_image" className="object-none" />
+      <Image
+        src={LandingImg}
+        alt="about_us_image"
+        className="mx-auto xl:object-none"
+      />
     </div>
   );
 };
